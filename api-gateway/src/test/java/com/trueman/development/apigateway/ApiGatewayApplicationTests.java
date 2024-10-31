9861ea7cbd5fe90f27ee6b31716746ac2a992a06
Kuznetsov20 <andrusha02.2002@mail.ru>
2024-10-31 03:42:37 +0300

    add-api-gateway

---
+++ api-gateway/src/test/java/com/trueman/development/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trueman.development.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
